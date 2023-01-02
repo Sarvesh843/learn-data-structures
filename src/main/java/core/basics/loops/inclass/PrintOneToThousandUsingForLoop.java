256f5122ac098da5c313bef4a20c6752b49ac95e
Hardik Khandelwal <khandelwal.hardik14@gmail.com>
2023-01-02 22:48:09 +0530

    Bidding assignment question

---
+++ src/main/java/core/basics/loops/inclass/PrintOneToThousandUsingForLoop.java
@@ -0,0 +1,14 @@
+package core.basics.loops.inclass;
+
+public class PrintOneToThousandUsingForLoop {
+    public static void main(String[] args) {
+        /**
+         * Print numbers between 1 to 1000
+         * using for loop.
+         */
+        for (int i = 1; i <= 1000;  i++){
+            System.out.println(i);
+        }
+
+    }
+}
