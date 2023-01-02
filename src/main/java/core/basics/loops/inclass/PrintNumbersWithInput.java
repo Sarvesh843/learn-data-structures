579a8294c107c4c4cdb7e51a85bdee1ce2235a1a
Hardik Khandelwal <khandelwal.hardik14@gmail.com>
2023-01-02 21:46:11 +0530

    Print numbers with input

---
+++ src/main/java/core/basics/loops/inclass/PrintNumbersWithInput.java
@@ -0,0 +1,17 @@
+package core.basics.loops.inclass;
+
+public class PrintNumbersWithInput {
+    public static void main(String[] args) {
+        /**
+         * Write a program which takes an integer
+         * n as input, and prints the numbers between
+         * 1 2 3 4 5 ......n on the same line
+         * separated by space.
+         */
+        // TODO: Write code to input n
+
+        // Initialize code for loop
+
+        // Write Loop with condition and update logic
+    }
+}
