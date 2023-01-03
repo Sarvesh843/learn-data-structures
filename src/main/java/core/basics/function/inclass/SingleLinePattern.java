c340b11cc26df131876f7cf3e7951e0c94963c61
Hardik Khandelwal <khandelwal.hardik14@gmail.com>
2023-01-03 21:51:01 +0530

    Patterns loop

---
+++ src/main/java/core/basics/function/inclass/SingleLinePattern.java
@@ -0,0 +1,29 @@
+package core.basics.function.inclass;
+
+public class SingleLinePattern {
+
+    /**
+     *  Given some n, can you print this pattern
+     *
+     * 1 2 3 4 5 6 7 8 .. ..... n
+     *
+     * example:
+     * n = 3
+     * 1 2 3
+     *
+     * n =  4
+     * 1 2 3 4
+     *
+     * n= 5
+     * 1 2 3 4 5
+     */
+    static void printRow(int n){
+        for (int i=1; i <= n ; i++){
+            System.out.print(i+" ");
+        }
+    }
+
+    public static void main(String[] args) {
+        printRow(19);
+    }
+}
---
+++ src/main/java/core/basics/function/inclass/SingleLinePatternDescending.java
@@ -0,0 +1,31 @@
+package core.basics.function.inclass;
+
+public class SingleLinePatternDescending {
+
+    /**
+     *  Given some n, can you print this pattern
+     *
+     * print in descending
+     *
+     * n n-1 n-2  ....... 1
+     *
+     * example:
+     * n = 3
+     * 3 2 1
+     *
+     * n =  4
+     * 4 3 2 1
+     *
+     * n= 5
+     * 5 4 3 2 1
+     */
+    static void printDesc(int n){
+
+    }
+
+
+
+    public static void main(String[] args) {
+        printDesc(9);
+    }
+}
