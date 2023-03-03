e839f8f629bb179f22cc59930b15fd2200d2b771
Hardik Khandelwal <khandelwal.hardik14@gmail.com>
2023-03-03 21:14:24 +0530

    OOPS: Storing customer data separaterly

---
+++ src/main/java/oops/banking/Bank.java
@@ -0,0 +1,18 @@
+package oops.banking;
+
+public class Bank {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Children bank of India!");
+
+        String customer1 = "Kunal";
+        int ageCustomer1 = 22;
+        String parentNameCustomer1 = "parentKunal";
+        String occupationCustomer1 = "student";
+
+        String customer2 = "Ashwath";
+        int ageCustomer2 = 23;
+        String parentNameCustomer2 = "parentAshwath";
+        String occupationCustomer2 = "student";
+
+    }
+}
