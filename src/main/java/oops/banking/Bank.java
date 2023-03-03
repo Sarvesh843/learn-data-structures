03356c060b7453c572f340d1440e1e663d45a082
Hardik Khandelwal <khandelwal.hardik14@gmail.com>
2023-03-03 21:27:11 +0530

    Creating Customer class

--- src/main/java/oops/banking/Bank.java
+++ src/main/java/oops/banking/Bank.java
@@ -4,15 +4,20 @@ public class Bank {
     public static void main(String[] args) {
         System.out.println("Welcome to Children bank of India!");
 
-        String customer1 = "Kunal";
-        int ageCustomer1 = 22;
-        String parentNameCustomer1 = "parentKunal";
-        String occupationCustomer1 = "student";
+        Customer c1 = new Customer(
+                1,
+                "Ashwath",
+                23,
+                "student");
 
-        String customer2 = "Ashwath";
-        int ageCustomer2 = 23;
-        String parentNameCustomer2 = "parentAshwath";
-        String occupationCustomer2 = "student";
+        Customer c2 = new Customer(
+                2,
+                "Kunal",
+                22,
+                "student");
+
+        System.out.println(c1.name);
+        System.out.println(c2.name);
 
     }
 }
